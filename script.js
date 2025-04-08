792627d2502984b0923ceda5b98c4c23ddb477e8
riccardo <riccardo.masetti@outlook.com>
2025-04-08 19:25:55 +0200

    Initial Commit

---
+++ script.js
@@ -0,0 +1,14 @@
+
+// NON SERVE?
+document.querySelectorAll('.nav-link').forEach(link => {
+    link.addEventListener('click', function (e) {
+      // Rimuove la classe dagli altri
+      document.querySelectorAll('.nav-link').forEach(el => {
+        el.classList.remove('clicked');
+      });
+      // Aggiunge la classe a quello cliccato
+      this.classList.add('clicked');
+    });
+  });
+
+  
